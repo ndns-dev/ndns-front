8baadb684bc304d67089e5105782fcc9e177f67a
sh5080 <prpn97@gmail.com>
2025-05-11 19:52:51 +0900

    feat: radix-ui 사용 ui 컴포넌트 세팅

---
+++ src/components/ui/badge.component.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/utils/class-name.util";
+
+const badgeVariants = cva(
+  "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2",
+  {
+    variants: {
+      variant: {
+        default:
+          "border-transparent bg-emerald-500 text-white hover:bg-emerald-600",
+        secondary:
+          "border-transparent bg-blue-100 text-blue-800 hover:bg-blue-200",
+        destructive:
+          "border-transparent bg-red-500 text-white hover:bg-red-600",
+        outline: "text-emerald-700",
+        green:
+          "border-transparent bg-green-100 text-green-800 hover:bg-green-200",
+        blue: "border-transparent bg-blue-100 text-blue-800 hover:bg-blue-200",
+        red: "border-transparent bg-red-100 text-red-800 hover:bg-red-200",
+        yellow:
+          "border-transparent bg-yellow-100 text-yellow-800 hover:bg-yellow-200",
+        purple:
+          "border-transparent bg-purple-100 text-purple-800 hover:bg-purple-200",
+        pink: "border-transparent bg-pink-100 text-pink-800 hover:bg-pink-200",
+        indigo:
+          "border-transparent bg-indigo-100 text-indigo-800 hover:bg-indigo-200",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {}
+
+function Badge({ className, variant, ...props }: BadgeProps) {
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+  );
+}
+
+export { Badge, badgeVariants };
