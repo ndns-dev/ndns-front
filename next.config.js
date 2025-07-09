8aa6bfe83288f5e63c30a570e776d1f1df143a1f
sh5080 <prpn97@gmail.com>
2025-07-09 15:33:35 +0900

    feat: client side만 사용하는것으로 결정, output export

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
     domains: ['blogfiles.naver.net', 'postfiles.pstatic.net'],
